50a641eee05ecc00fbb2a0a79b972d1d191c65ba
Joaquin <Joaquin@DESKTOP-1E6358C>
2024-08-28 17:55:06 -0300

    diseño del formulario

--- Inventario de motores/Form1.Designer.cs
+++ Inventario de motores/Form1.Designer.cs
@@ -28,20 +28,87 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.btn_Ingresar = new System.Windows.Forms.Button();
+            this.lbl_Usuario = new System.Windows.Forms.Label();
+            this.lbl_Contraseña = new System.Windows.Forms.Label();
+            this.txt_Usuario = new System.Windows.Forms.TextBox();
+            this.txt_Contraseña = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
+            // 
+            // btn_Ingresar
+            // 
+            this.btn_Ingresar.BackColor = System.Drawing.Color.Lime;
+            this.btn_Ingresar.Font = new System.Drawing.Font("Malgun Gothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Ingresar.Location = new System.Drawing.Point(136, 203);
+            this.btn_Ingresar.Name = "btn_Ingresar";
+            this.btn_Ingresar.Size = new System.Drawing.Size(103, 39);
+            this.btn_Ingresar.TabIndex = 0;
+            this.btn_Ingresar.Text = "Ingresar";
+            this.btn_Ingresar.UseVisualStyleBackColor = false;
+            // 
+            // lbl_Usuario
+            // 
+            this.lbl_Usuario.AutoSize = true;
+            this.lbl_Usuario.Font = new System.Drawing.Font("Malgun Gothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Usuario.Location = new System.Drawing.Point(158, 37);
+            this.lbl_Usuario.Name = "lbl_Usuario";
+            this.lbl_Usuario.Size = new System.Drawing.Size(56, 17);
+            this.lbl_Usuario.TabIndex = 1;
+            this.lbl_Usuario.Text = "Usuario";
+            // 
+            // lbl_Contraseña
+            // 
+            this.lbl_Contraseña.AutoSize = true;
+            this.lbl_Contraseña.Font = new System.Drawing.Font("Malgun Gothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Contraseña.Location = new System.Drawing.Point(147, 108);
+            this.lbl_Contraseña.Name = "lbl_Contraseña";
+            this.lbl_Contraseña.Size = new System.Drawing.Size(77, 17);
+            this.lbl_Contraseña.TabIndex = 2;
+            this.lbl_Contraseña.Text = "Contraseña";
+            // 
+            // txt_Usuario
+            // 
+            this.txt_Usuario.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txt_Usuario.Font = new System.Drawing.Font("Malgun Gothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_Usuario.Location = new System.Drawing.Point(136, 69);
+            this.txt_Usuario.Name = "txt_Usuario";
+            this.txt_Usuario.Size = new System.Drawing.Size(100, 25);
+            this.txt_Usuario.TabIndex = 3;
+            // 
+            // txt_Contraseña
+            // 
+            this.txt_Contraseña.Font = new System.Drawing.Font("Malgun Gothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_Contraseña.Location = new System.Drawing.Point(136, 137);
+            this.txt_Contraseña.Name = "txt_Contraseña";
+            this.txt_Contraseña.Size = new System.Drawing.Size(100, 25);
+            this.txt_Contraseña.TabIndex = 4;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(711, 456);
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(382, 263);
+            this.Controls.Add(this.txt_Contraseña);
+            this.Controls.Add(this.txt_Usuario);
+            this.Controls.Add(this.lbl_Contraseña);
+            this.Controls.Add(this.lbl_Usuario);
+            this.Controls.Add(this.btn_Ingresar);
+            this.DoubleBuffered = true;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Inventario de motores";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btn_Ingresar;
+        private System.Windows.Forms.Label lbl_Usuario;
+        private System.Windows.Forms.Label lbl_Contraseña;
+        private System.Windows.Forms.TextBox txt_Usuario;
+        private System.Windows.Forms.TextBox txt_Contraseña;
     }
 }
 
