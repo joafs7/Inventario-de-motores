92da88bbd009a513fa64639b77ac3c5de7022d6b
joafs7 <fabiansantillan2000@gmail.com>
2024-12-23 20:24:45 -0300

    Motores de clientes ya producido

--- Inventario de motores/Motores de clientes.Designer.cs
+++ Inventario de motores/Motores de clientes.Designer.cs
@@ -38,7 +38,7 @@
             this.txt_Cliente = new System.Windows.Forms.TextBox();
             this.lbl_Marca = new System.Windows.Forms.Label();
             this.cbx_Marca = new System.Windows.Forms.ComboBox();
-            this.cbx_Hp = new System.Windows.Forms.ComboBox();
+            this.cbx_HP = new System.Windows.Forms.ComboBox();
             this.lbl_HP = new System.Windows.Forms.Label();
             this.cbx_RPM = new System.Windows.Forms.ComboBox();
             this.lbl_RPM = new System.Windows.Forms.Label();
@@ -68,6 +68,7 @@
             this.btn_Volver.TabIndex = 0;
             this.btn_Volver.Text = "Volver";
             this.btn_Volver.UseVisualStyleBackColor = false;
+            this.btn_Volver.Click += new System.EventHandler(this.btn_Volver_Click);
             // 
             // btn_Nuevo
             // 
@@ -78,6 +79,7 @@
             this.btn_Nuevo.TabIndex = 1;
             this.btn_Nuevo.Text = "Nuevo";
             this.btn_Nuevo.UseVisualStyleBackColor = false;
+            this.btn_Nuevo.Click += new System.EventHandler(this.btn_Nuevo_Click);
             // 
             // btn_Modificar
             // 
@@ -88,6 +90,7 @@
             this.btn_Modificar.TabIndex = 2;
             this.btn_Modificar.Text = "Modificar";
             this.btn_Modificar.UseVisualStyleBackColor = false;
+            this.btn_Modificar.Click += new System.EventHandler(this.btn_Modificar_Click);
             // 
             // btn_Eliminar
             // 
@@ -98,6 +101,7 @@
             this.btn_Eliminar.TabIndex = 3;
             this.btn_Eliminar.Text = "Eliminar";
             this.btn_Eliminar.UseVisualStyleBackColor = false;
+            this.btn_Eliminar.Click += new System.EventHandler(this.btn_Eliminar_Click);
             // 
             // btn_Guardar
             // 
@@ -108,6 +112,7 @@
             this.btn_Guardar.TabIndex = 4;
             this.btn_Guardar.Text = "Guardar";
             this.btn_Guardar.UseVisualStyleBackColor = false;
+            this.btn_Guardar.Click += new System.EventHandler(this.btn_Guardar_Click);
             // 
             // lbl_Cliente
             // 
@@ -156,11 +161,11 @@
             this.cbx_Marca.Size = new System.Drawing.Size(154, 28);
             this.cbx_Marca.TabIndex = 8;
             // 
-            // cbx_Hp
+            // cbx_HP
             // 
-            this.cbx_Hp.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.cbx_Hp.FormattingEnabled = true;
-            this.cbx_Hp.Items.AddRange(new object[] {
+            this.cbx_HP.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbx_HP.FormattingEnabled = true;
+            this.cbx_HP.Items.AddRange(new object[] {
             "0.25",
             "0.33",
             "0.5",
@@ -183,10 +188,10 @@
             "70",
             "100",
             "125"});
-            this.cbx_Hp.Location = new System.Drawing.Point(543, 203);
-            this.cbx_Hp.Name = "cbx_Hp";
-            this.cbx_Hp.Size = new System.Drawing.Size(162, 28);
-            this.cbx_Hp.TabIndex = 10;
+            this.cbx_HP.Location = new System.Drawing.Point(543, 203);
+            this.cbx_HP.Name = "cbx_HP";
+            this.cbx_HP.Size = new System.Drawing.Size(162, 28);
+            this.cbx_HP.TabIndex = 10;
             // 
             // lbl_HP
             // 
@@ -334,6 +339,7 @@
             this.txt_FiltroCliente.Name = "txt_FiltroCliente";
             this.txt_FiltroCliente.Size = new System.Drawing.Size(354, 30);
             this.txt_FiltroCliente.TabIndex = 22;
+            this.txt_FiltroCliente.TextChanged += new System.EventHandler(this.txt_FiltroCliente_TextChanged);
             // 
             // dgv_Cliente
             // 
@@ -356,6 +362,7 @@
             this.btn_Imprimir.Size = new System.Drawing.Size(177, 113);
             this.btn_Imprimir.TabIndex = 23;
             this.btn_Imprimir.UseVisualStyleBackColor = false;
+            this.btn_Imprimir.Click += new System.EventHandler(this.btn_Imprimir_Click);
             // 
             // btn_ExportarPDF
             // 
@@ -368,6 +375,7 @@
             this.btn_ExportarPDF.Size = new System.Drawing.Size(177, 113);
             this.btn_ExportarPDF.TabIndex = 24;
             this.btn_ExportarPDF.UseVisualStyleBackColor = false;
+            this.btn_ExportarPDF.Click += new System.EventHandler(this.btn_ExportarPDF_Click);
             // 
             // Motores_de_clientes
             // 
@@ -389,7 +397,7 @@
             this.Controls.Add(this.lbl_Volts);
             this.Controls.Add(this.cbx_RPM);
             this.Controls.Add(this.lbl_RPM);
-            this.Controls.Add(this.cbx_Hp);
+            this.Controls.Add(this.cbx_HP);
             this.Controls.Add(this.lbl_HP);
             this.Controls.Add(this.cbx_Marca);
             this.Controls.Add(this.lbl_Marca);
@@ -403,6 +411,7 @@
             this.Cursor = System.Windows.Forms.Cursors.Default;
             this.Name = "Motores_de_clientes";
             this.Text = "Motores_de_clientes";
+            this.Load += new System.EventHandler(this.Motores_de_clientes_Load);
             this.gbx_Cliente.ResumeLayout(false);
             this.gbx_Cliente.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dgv_Cliente)).EndInit();
@@ -422,7 +431,7 @@
         private System.Windows.Forms.TextBox txt_Cliente;
         private System.Windows.Forms.Label lbl_Marca;
         private System.Windows.Forms.ComboBox cbx_Marca;
-        private System.Windows.Forms.ComboBox cbx_Hp;
+        private System.Windows.Forms.ComboBox cbx_HP;
         private System.Windows.Forms.Label lbl_HP;
         private System.Windows.Forms.ComboBox cbx_RPM;
         private System.Windows.Forms.Label lbl_RPM;
--- Inventario de motores/Motores de clientes.cs
+++ Inventario de motores/Motores de clientes.cs
@@ -7,14 +7,411 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Drawing.Printing;
+using System.IO;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+using System.Data.SqlClient;
 
 namespace Inventario_de_motores
 {
     public partial class Motores_de_clientes : Form
     {
+        private PrintDocument printDocument = new PrintDocument();
+        private int currentRow = 0;
         public Motores_de_clientes()
         {
             InitializeComponent();
+            // Configurar el evento para imprimir
+            printDocument.BeginPrint += PrintDocument_BeginPrint;
+            printDocument.PrintPage += PrintDocument_PrintPage;
+        }
+
+        public string guardar = "Nuevo";
+        public int codigo;
+        public SqlConnection Conexion = new SqlConnection("Data Source=JOAQUIN;Initial Catalog=Gatti_DB;Integrated Security=True;Encrypt=False");
+
+        private void Nuevo()
+        {
+            //Nuevo="Nuevo"
+            //habilitamos botones 
+
+            btn_Modificar.Enabled = true;
+            btn_Eliminar.Enabled = true;
+            btn_Guardar.Enabled = true;
+
+            //habilitamos cajas de textos
+            txt_Cantidad.Enabled = true;
+            cbx_Marca.Enabled = true;
+            cbx_HP.Enabled = true;
+            cbx_RPM.Enabled = true;
+            cbx_Cuerpo.Enabled = true;
+            cbx_Hz.Enabled = true;
+            cbx_Volts.Enabled = true;
+        }
+
+        private void LimpiarCampos()
+        {
+            txt_Cantidad.Text = string.Empty;
+            cbx_Marca.SelectedIndex = 0;
+            cbx_HP.SelectedIndex = 0;
+            cbx_RPM.SelectedIndex = 0;
+            cbx_Cuerpo.SelectedIndex = 0;
+            cbx_Hz.SelectedIndex = 0;
+            cbx_Volts.SelectedIndex = 0;
+        }
+        private void CargarGrilla()
+        {
+            try
+            {
+                Conexion.Open();
+
+                // Consulta SQL
+                string consultaSQL = "SELECT * FROM motorDeLinea";
+                SqlCommand comando = new SqlCommand(consultaSQL, Conexion);
+
+                // Cargar datos en DataTable
+                SqlDataAdapter adapter = new SqlDataAdapter(comando);
+                DataTable datosGrilla = new DataTable();
+                adapter.Fill(datosGrilla);
+
+                // Asignar datos al DataGridView
+                dgv_Cliente.DataSource = datosGrilla;
+                Conexion.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error al cargar los datos en la grilla: {ex.Message}", "Error");
+            }
+        }
+        private void btn_Nuevo_Click(object sender, EventArgs e)
+        {
+            Nuevo();
+        }
+
+        private void btn_Guardar_Click(object sender, EventArgs e)
+        {
+            if (cbx_Marca.Text == "Seleccione")
+            {
+                MessageBox.Show("Debe seleccionar 'Marca' del motor", "Dato requerido");
+                cbx_Marca.Focus();
+                return;
+            }
+
+            if (cbx_HP.Text == "Seleccione")
+            {
+                MessageBox.Show("Debe seleccionar 'HP' del motor", "Dato requerido");
+                cbx_HP.Focus();
+                return;
+            }
+
+            if (cbx_RPM.Text == "Seleccione")
+            {
+                MessageBox.Show("Debe seleccionar 'RPM' del motor", "Dato requerido");
+                cbx_RPM.Focus();
+                return;
+            }
+
+            if (txt_Cantidad.Text == string.Empty)
+            {
+                MessageBox.Show("Debe colocar la 'cantidad' de motores a ingresar", "Dato requerido");
+                txt_Cantidad.Focus();
+                return;
+            }
+
+            if (cbx_Cuerpo.Text == "Seleccione")
+            {
+                MessageBox.Show("Debe seleccionar 'Cuerpo' del motor", "Dato requerido");
+                cbx_Cuerpo.Focus();
+                return;
+            }
+
+            if (cbx_Volts.Text == "Seleccione")
+            {
+                MessageBox.Show("Debe seleccionar 'Volts' del motor", "Dato requerido");
+                cbx_Volts.Focus();
+                return;
+            }
+
+            if (cbx_Hz.Text == "Seleccione")
+            {
+                MessageBox.Show("Debe seleccionar 'Hz' del motor", "Dato requerido");
+                cbx_Hz.Focus();
+                return;
+            }
+
+            string cliente= txt_Cliente.Text;
+            string marca = cbx_Marca.Text;
+            string hp = cbx_HP.Text;
+            string rpm = cbx_RPM.Text;
+            string cantidad = txt_Cantidad.Text;
+            string cuerpo = cbx_Cuerpo.Text;
+            string volts = cbx_Volts.Text;
+            string hz = cbx_Hz.Text;
+
+            Conexion.Open();
+            try
+            {
+                SqlCommand comandoSQL;
+                if (guardar == "Nuevo")
+                {
+                    comandoSQL = new SqlCommand("INSERT INTO MotorDeCliente (cliente,marca,hp,rpm,cantidad,cuerpo,volts,hz)" + "VALUES (@Cliente, @Marca,@HP,@RPM,@Cantidad,@Cuerpo,@Volts,@Hz)", Conexion);
+                    comandoSQL.Parameters.AddWithValue("@Cliente", cliente);
+                    comandoSQL.Parameters.AddWithValue("@Marca", marca);
+                    comandoSQL.Parameters.AddWithValue("@HP", hp);
+                    comandoSQL.Parameters.AddWithValue("@RPM", rpm);
+                    comandoSQL.Parameters.AddWithValue("@Cantidad", cantidad);
+                    comandoSQL.Parameters.AddWithValue("@Cuerpo", cuerpo);
+                    comandoSQL.Parameters.AddWithValue("@Volts", volts);
+                    comandoSQL.Parameters.AddWithValue("@Hz", hz);
+                    comandoSQL.ExecuteNonQuery();
+                    MessageBox.Show("Motores cargados correctamente");
+
+                }
+                else if (guardar == "Modificar")
+                {
+                    comandoSQL = new SqlCommand("UPDATE MotorDeCliente SET cliente=@Cliente marca=@Marca, hp=@HP, rpm=@RPM, cantidad=@Cantidad, cuerpo=@Cuerpo, volts=@Volts, hz=@Hz WHERE codigo=@Codigo", Conexion);
+                    comandoSQL.Parameters.AddWithValue("@Cliente", cliente);
+                    comandoSQL.Parameters.AddWithValue("@Marca", marca);
+                    comandoSQL.Parameters.AddWithValue("@HP", hp);
+                    comandoSQL.Parameters.AddWithValue("@RPM", rpm);
+                    comandoSQL.Parameters.AddWithValue("@Cantidad", cantidad);
+                    comandoSQL.Parameters.AddWithValue("@Cuerpo", cuerpo);
+                    comandoSQL.Parameters.AddWithValue("@Volts", volts);
+                    comandoSQL.Parameters.AddWithValue("@Hz", hz);
+                    comandoSQL.Parameters.AddWithValue("@Codigo", codigo);
+                    comandoSQL.ExecuteNonQuery();
+                    MessageBox.Show("Motores actualizados exitosamente");
+
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("ERROR:" + ex.Message, "ERROR");
+            }
+            finally
+            {
+
+                Conexion.Close();
+                LimpiarCampos();
+                CargarGrilla();
+            }
+
+        }
+
+        private void btn_Modificar_Click(object sender, EventArgs e)
+        {
+            if (dgv_Cliente.SelectedRows.Count > 0)
+            {
+                foreach (DataGridViewRow row in dgv_Cliente.SelectedRows)
+                {
+                    codigo = Convert.ToInt32(row.Cells["Codigo"].Value); // Capturamos el ID del cliente seleccionado
+                    txt_Cliente.Text = row.Cells["Cliente"].Value.ToString();
+                    cbx_Marca.Text = row.Cells["Marca"].Value.ToString();
+                    cbx_HP.Text = row.Cells["HP"].Value.ToString();
+                    cbx_RPM.Text = row.Cells["RPM"].Value.ToString();
+                    cbx_Cuerpo.Text = row.Cells["Cuerpo"].Value.ToString();
+                    txt_Cantidad.Text = row.Cells["Cantidad"].Value.ToString();
+                    cbx_Volts.Text = row.Cells["Volts"].Value.ToString();
+                    cbx_Hz.Text = row.Cells["Hz"].Value.ToString();
+                    guardar = "Modificar";
+                    Nuevo();
+                }
+
+            }
+            else
+            {
+                //No hay filas seleccionadas
+                MessageBox.Show("Debe seleccionar un 'MOTOR'", "Seleccione");
+            }
+
+        }
+
+        private void btn_Eliminar_Click(object sender, EventArgs e)
+        {
+            if (dgv_Cliente.SelectedRows.Count > 0)
+            {
+                foreach (DataGridViewRow row in dgv_Cliente.SelectedRows)
+                {
+                    int codigo = Convert.ToInt32(row.Cells["Codigo"].Value);
+
+                    Conexion.Open();
+                    SqlCommand comandoSQL;
+                    comandoSQL = new SqlCommand("DELETE FROM MotorDeCliente WHERE codigo = @Codigo", Conexion);
+                    comandoSQL.Parameters.AddWithValue("@Codigo", codigo);
+                    comandoSQL.ExecuteNonQuery();
+                    Conexion.Close();
+                    CargarGrilla();
+                    MessageBox.Show("Motores eliminado exitosamente", "Eliminacion");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Debe seleccionar Motores", "Seleccione");
+            }
+        }
+
+        private void Motores_de_clientes_Load(object sender, EventArgs e)
+        {
+            CargarGrilla();
+            LimpiarCampos();
+        }
+
+        private void btn_Volver_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Menu_Principal menu_Principal = new Menu_Principal();
+            menu_Principal.Show();
+        }
+
+        private void txt_FiltroCliente_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                Conexion.Open();
+
+                // Captura el texto en el textbox
+                string filtro = txt_FiltroCliente.Text;
+
+                // Crea la consulta SQL con el filtro 
+                string consulta = "SELECT * FROM MotorDeCliente WHERE Cliente LIKE @filtro";
+                SqlCommand comandoSQL = new SqlCommand(consulta, Conexion);
+                comandoSQL.Parameters.AddWithValue("@filtro", "%" + filtro + "%");
+
+                //Ejecuta la consulta y obtiene los resultados
+                SqlDataAdapter adaptor = new SqlDataAdapter(comandoSQL);
+                DataTable tablaResultados = new DataTable();
+                adaptor.Fill(tablaResultados);
+
+                //Muestra los resultados de la DataGridView
+                dgv_Cliente.DataSource = tablaResultados;
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ocurrio un error: " + ex.Message, "Error");
+            }
+            finally
+            {
+                Conexion.Close();
+            }
+        }
+
+        private void btn_Imprimir_Click(object sender, EventArgs e)
+        {
+            PrintPreviewDialog previewDialog = new PrintPreviewDialog();
+            previewDialog.Document = printDocument;
+            previewDialog.ShowDialog();
+        }
+        private void PrintDocument_BeginPrint(object sender, PrintEventArgs e)
+        {
+            currentRow = 0; // Reiniciar la fila actual
+        }
+        private void PrintDocument_PrintPage(object sender, PrintPageEventArgs e)
+        {
+            int x = 50; // Margen izquierdo
+            int y = 50; // Margen superior
+            int rowHeight = 25; // Altura de las filas
+            int colWidth = 100; // Ancho de las columnas
+            int maxRowsPerPage = (e.MarginBounds.Height - y) / rowHeight;
+
+            System.Drawing.Font headerFont = new System.Drawing.Font("Arial", 10, FontStyle.Bold);
+            System.Drawing.Font rowFont = new System.Drawing.Font("Arial", 9, FontStyle.Regular);
+
+            // Dibujar los encabezados de columna
+            for (int col = 0; col < dgv_Cliente.Columns.Count; col++)
+            {
+                e.Graphics.DrawString(dgv_Cliente.Columns[col].HeaderText, headerFont, Brushes.Black, x + (col * colWidth), y);
+            }
+
+            y += rowHeight;
+
+            // Dibujar las filas de datos
+            for (; currentRow < dgv_Cliente.Rows.Count; currentRow++)
+            {
+                if (y + rowHeight > e.MarginBounds.Bottom)
+                {
+                    e.HasMorePages = true;
+                    return; // Salir para continuar en la siguiente página
+                }
+
+                for (int col = 0; col < dgv_Cliente.Columns.Count; col++)
+                {
+                    string cellValue = dgv_Cliente.Rows[currentRow].Cells[col].Value?.ToString() ?? "";
+                    e.Graphics.DrawString(cellValue, rowFont, Brushes.Black, x + (col * colWidth), y);
+                }
+
+                y += rowHeight;
+            }
+
+            e.HasMorePages = false;
+        }
+
+        private void btn_ExportarPDF_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog
+            {
+                Filter = "Archivo PDF|*.pdf",
+                Title = "Guardar como PDF",
+                FileName = "DataGridViewExport.pdf"
+            };
+
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                ExportarPDF(saveFileDialog.FileName);
+            }
+        }
+        private void ExportarPDF(string nombreArchivo)
+        {
+            Document documento = new Document(PageSize.A4, 10, 10, 10, 10);
+
+            try
+            {
+                PdfWriter.GetInstance(documento, new FileStream(nombreArchivo, FileMode.Create));
+                documento.Open();
+
+                // Título
+                documento.Add(new Paragraph("Motores de Clientes"));
+                documento.Add(new Paragraph(" ")); // Espacio en blanco
+
+                PdfPTable tabla = new PdfPTable(dgv_Cliente.Columns.Count);
+                tabla.WidthPercentage = 100;
+
+                // Encabezados
+                foreach (DataGridViewColumn columna in dgv_Cliente.Columns)
+                {
+                    PdfPCell celda = new PdfPCell(new Phrase(columna.HeaderText))
+                    {
+                        BackgroundColor = BaseColor.LIGHT_GRAY,
+                        HorizontalAlignment = Element.ALIGN_CENTER
+                    };
+                    tabla.AddCell(celda);
+                }
+
+                // Filas
+                foreach (DataGridViewRow fila in dgv_Cliente.Rows)
+                {
+                    if (!fila.IsNewRow)
+                    {
+                        foreach (DataGridViewCell celda in fila.Cells)
+                        {
+                            string textoCelda = celda.Value?.ToString() ?? "";
+                            tabla.AddCell(new Phrase(textoCelda));
+                        }
+                    }
+                }
+
+                documento.Add(tabla);
+                MessageBox.Show("Archivo PDF generado exitosamente.", "Éxito");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al generar el archivo PDF: " + ex.Message, "Error");
+            }
+            finally
+            {
+                documento.Close();
+            }
         }
     }
 }
